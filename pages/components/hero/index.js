43b0cbb19176b45507eee151457ffb0b807dc609
velarded <danica.cristine.velarde@gmail.com>
2022-02-10 22:52:05 -0500

    fix hero description

--- pages/components/hero/index.js
+++ pages/components/hero/index.js
@@ -13,11 +13,11 @@ export default function Hero() {
           ></ion-icon>
         </h1>
         <p className={styles.textDescription}>
-          I am a Software Developer from in Toronto, Canada. I aspire to become
-          a Full-Stack Developer and create websites which can be helpful to
+          I am a Software Developer in Toronto, Canada. I aspire to become a
+          Full-Stack Developer and create websites which can be helpful to
           people in their everyday lives. During the COVID-19 pandemic, I
-          decided to pick up React to start making some website ideas of mine
-          become a reality.
+          decided to pick up React to start making ideas of mine become a
+          reality.
           <br />
           Feel free to browse the site to get to know me more!
         </p>
